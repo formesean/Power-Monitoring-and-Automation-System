a0c688386c2ad925b06319e59b900ad0cb3a7f9d
formesean <seanaguilar698@gmail.com>
2025-04-28 19:56:48 +0800

    first commit

---
+++ src/main.c
@@ -0,0 +1,32 @@
+#include <xc.h> // include file for the XC8 compiler
+#include <stdio.h>
+
+#include "I2C.h"
+#include "I2C_LCD.h"
+
+#pragma config FOSC = XT
+#pragma config WDTE = OFF
+#pragma config PWRTE = ON
+#pragma config BOREN = ON
+#pragma config LVP = OFF
+#pragma config CPD = OFF
+#pragma config WRT = OFF
+#pragma config CP = OFF
+
+#define _XTAL_FREQ 4000000
+
+void main(void)
+{
+	init_I2C_Master();
+	LCD_Init(0x4E);
+	
+	LCD_Set_Cursor(1, 1);
+  LCD_Write_String("Hello");
+  LCD_Set_Cursor(2, 1);
+  LCD_Write_String("World!");
+	
+
+  while (1)
+  {
+	}
+}
